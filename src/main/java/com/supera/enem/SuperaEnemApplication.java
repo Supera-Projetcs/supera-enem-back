3f47eb82d3dd5880b2d21ec301e6337ccb493cf2
Matheus Bento <matheus51bento@gmail.com>
2024-11-25 17:19:02 -0300

    initial

---
+++ src/main/java/com/supera/enem/SuperaEnemApplication.java
@@ -0,0 +1,13 @@
+package com.supera.enem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperaEnemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperaEnemApplication.class, args);
+	}
+
+}
