819337d2511bec5a0d0914d9ad649105775048c9
michaelcesar <michaelcesarflopesgit@gmail.com>
2025-01-07 21:56:51 -0300

    feat: Test Controller

---
+++ src/main/java/com/supera/enem/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.supera.enem.controller;
+
+import com.supera.enem.controller.DTOS.TestResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tests")
+public class TestController {
+    @Autowired
+    private TestService testService;
+
+    @GetMapping("/completed")
+    public List<TestResponseDTO> getCompletedTests() {
+        return testService.getCompletedTests();
+    }
+}
