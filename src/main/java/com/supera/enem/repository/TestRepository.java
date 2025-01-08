74949b976511846d232d8a597dbb73f22c641f2b
michaelcesar <michaelcesarflopesgit@gmail.com>
2025-01-07 21:58:38 -0300

    feat: Test repository

---
+++ src/main/java/com/supera/enem/repository/TestRepository.java
@@ -0,0 +1,14 @@
+package com.supera.enem.repository;
+
+import com.supera.enem.domain.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, Long> {
+    @Query("SELECT t FROM Test t WHERE t.date <= CURRENT_DATE")
+    List<Test> findCompletedTests();
+}
