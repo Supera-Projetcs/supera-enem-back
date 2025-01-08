e96ed76be26e154ed0c3d8324fe7765b6e92a17e
michaelcesar <michaelcesarflopesgit@gmail.com>
2025-01-07 21:58:57 -0300

    feat: Test service

--- src/main/java/com/supera/enem/controller/TestController.java
+++ src/main/java/com/supera/enem/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.supera.enem.controller;
 
 import com.supera.enem.controller.DTOS.TestResponseDTO;
+import com.supera.enem.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/supera/enem/service/TestService.java
@@ -0,0 +1,24 @@
+package com.supera.enem.service;
+
+import com.supera.enem.controller.DTOS.TestResponseDTO;
+import com.supera.enem.domain.Test;
+import com.supera.enem.mapper.TestMapper;
+import com.supera.enem.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TestService {
+    @Autowired
+    private TestRepository testRepository;
+    private final TestMapper testMapper = TestMapper.INSTANCE;
+
+    public List<TestResponseDTO> getCompletedTests() {
+        return testRepository.findCompletedTests().stream()
+                .map(testMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+}
