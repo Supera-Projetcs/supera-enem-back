b58e961d4abf7401f53e5f62e85925076beb0e50
Matheus Bento <matheus51bento@gmail.com>
2025-02-12 08:36:32 -0300

    tests

--- src/test/java/com/supera/enem/service/KeyClockUserServiceTest.java
+++ src/test/java/com/supera/enem/service/KeyClockUserServiceTest.java
@@ -1,34 +1,52 @@
 package com.supera.enem.service;
 
 import com.supera.enem.controller.DTOS.UseKeycloakRegistrationDTO;
-import org.junit.Test;
+import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+
+@ExtendWith(MockitoExtension.class)
 public class KeyClockUserServiceTest {
+
+    @Mock
+    private Keycloak keycloak;
+    @Mock
+    private UsersResource usersResource;
 
     @InjectMocks
     private KeycloackUserService keycloackUserService;
 
-    private UseKeycloakRegistrationDTO userDTO;
-
-    @BeforeEach
-    public void setUp() {
-        userDTO = new UseKeycloakRegistrationDTO();
-        userDTO.setUsername("testuser");
-        userDTO.setEmail("test@example.com");
-        userDTO.setFirstName("Test");
-        userDTO.setLastName("User");
-        userDTO.setPassword("password123");
-
-
-    }
-
     @Test
-    @DisplayName("Test createUser")
-    public void testCreateUser() {
+    void testCreateUser_Success() {
+        UseKeycloakRegistrationDTO dto = new UseKeycloakRegistrationDTO("john_doe", "john@example.com", "John", "Doe", "password123");
+        Response response = mock(Response.class);
+        UserRepresentation userRepresentation = new UserRepresentation();
+        userRepresentation.setId("12345");
+        userRepresentation.setEmailVerified(false);
 
+        when(usersResource.create(any(UserRepresentation.class))).thenReturn(response);
+        when(response.getStatus()).thenReturn(201);
+        when(usersResource.searchByUsername(eq(dto.getUsername()), eq(true)))
+                .thenReturn(Collections.singletonList(userRepresentation));
+
+        String userId = keycloackUserService.createUser(dto);
+
+        assertEquals("12345", userId);
+        verify(usersResource).create(any(UserRepresentation.class));
     }
+
 
 }
