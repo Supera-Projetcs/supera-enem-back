660f4cd660cdc7d88538dcaaecefde4cdda8872c
Matheus Bento <matheus51bento@gmail.com>
2025-02-25 14:26:52 -0300

    testing answers controller

--- src/test/java/com/supera/enem/controller/AnswerControllerIntegrationTest.java
+++ src/test/java/com/supera/enem/controller/AnswerControllerIntegrationTest.java
@@ -1,22 +1,22 @@
 package com.supera.enem.controller;
 
 import com.supera.enem.controller.DTOS.AnswerRequestDTO;
-import com.supera.enem.domain.Answer;
-import com.supera.enem.domain.Question;
-import com.supera.enem.domain.Student;
-import com.supera.enem.domain.TestEntity;
+import com.supera.enem.controller.DTOS.AnswerResponseDTO;
+import com.supera.enem.domain.*;
 import com.supera.enem.domain.enums.TestType;
 import com.supera.enem.domain.enums.Weekday;
-import com.supera.enem.repository.AnswerRepository;
-import com.supera.enem.repository.QuestionRepository;
-import com.supera.enem.repository.StudentRepository;
-import com.supera.enem.repository.TestRepository;
+import com.supera.enem.repository.*;
+import com.supera.enem.service.AnswerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -27,10 +27,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -48,6 +52,8 @@ public class AnswerControllerIntegrationTest {
     @MockBean
     private JwtDecoder jwtDecoder;
 
+    private List<AnswerResponseDTO> answers;
+
     @Autowired
     private AnswerRepository answerRepository;
 
@@ -62,6 +68,8 @@ public class AnswerControllerIntegrationTest {
     private Student student;
     @Autowired
     private StudentRepository studentRepository;
+    @Autowired
+    private ContentRepository contentRepository;
 
     @Transactional
     @BeforeEach
@@ -85,9 +93,26 @@ public class AnswerControllerIntegrationTest {
         test.setStudent(student);
         testRepository.save(test);
 
+        Content content = new Content();
+        content.setName("Math Content");
+        content.setContent_weight(1.0);
+        content.setQuestion_weight(1.0);
+        contentRepository.save(content);
+
         question = new Question();
         question.setAnswer('A');
+        question.setContents(Set.of(content));
         questionRepository.save(question);
+
+//        AnswerResponseDTO answer1 = new AnswerResponseDTO();
+//        answer1.setId(1L);
+//        answer1.setCorrect(true);
+//
+//        AnswerResponseDTO answer2 = new AnswerResponseDTO();
+//        answer2.setId(2L);
+//        answer2.setCorrect(false);
+//
+//        answers = Arrays.asList(answer1, answer2);
     }
 
     private Jwt mockJwt() {
@@ -150,7 +175,7 @@ public class AnswerControllerIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .header("Authorization", "Bearer valid-token")
                         .content(objectMapper.writeValueAsString(answerRequest)))
-                .andExpect(status().isConflict());
+                .andExpect(status().isBadRequest());
     }
 
     @Test
@@ -171,4 +196,35 @@ public class AnswerControllerIntegrationTest {
                 .andExpect(status().isNotFound());
     }
 
+
+    @Test
+    @WithMockUser
+    public void testGetAnswersByTest_Success() throws Exception {
+        // Mock do JwtDecoder para retornar um JWT válido
+        Jwt jwt = mock(Jwt.class);
+        when(jwt.getClaim("sub")).thenReturn("keycloakUserId"); // Simula o subject do JWT
+        when(jwtDecoder.decode("valid-token")).thenReturn(jwt);
+
+        // Configura o SecurityContext para usar o JWT
+        JwtAuthenticationToken authentication = new JwtAuthenticationToken(jwt);
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        Long testId = 1L;
+
+        // Mock do repositório para retornar um teste
+        TestEntity test = new TestEntity();
+        test.setId(testId);
+        test.setStudent(student); // Associa o estudante ao teste
+//        when(testRepository.findById(testId)).thenReturn(Optional.of(test));
+
+        // Mock do repositório para retornar uma lista de respostas
+        List<Answer> answers = Arrays.asList(new Answer(), new Answer());
+//        when(answerRepository.findAll()).thenReturn(answers);
+
+        mockMvc.perform(get("/api/answers/{testId}", testId)
+                        .header("Authorization", "Bearer valid-token"))
+                .andExpect(status().isOk());
+    }
 }
