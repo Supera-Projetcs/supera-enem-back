54f49f1815894893cd9f37fc4bbf4178880a2aac
yasnnie <dev.cyas@gmail.com>
2025-02-26 15:30:39 -0300

    feat: cors habilitado para todos os dominios

--- src/main/java/com/supera/enem/config/WebSecurityConfig.java
+++ src/main/java/com/supera/enem/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -33,6 +34,11 @@ public class WebSecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(new JwtAuthenticationConverter()))
                 );
+
+
+
+        http.cors(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
