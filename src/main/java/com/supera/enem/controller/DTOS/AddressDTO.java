652a2709c3911b537534c3212f0c75d235366471
yasnnie <dev.cyas@gmail.com>
2025-02-27 13:55:30 -0300

    feat: bairro no model de endereço

--- src/main/java/com/supera/enem/controller/DTOS/AddressDTO.java
+++ src/main/java/com/supera/enem/controller/DTOS/AddressDTO.java
@@ -1,4 +1,5 @@
 package com.supera.enem.controller.DTOS;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
@@ -7,9 +8,16 @@ import lombok.*;
 @Setter
 @Getter
 public class AddressDTO {
+    @NotNull(message = "Rua não pode ser nulo.")
     private String street;
+    @NotNull(message = "Cidade não pode ser nulo.")
     private String city;
+    @NotNull(message = "Estado não pode ser nulo.")
     private String state;
+    @NotNull(message = "Bairro não pode ser nulo.")
+    private String neighborhood;
+    @NotNull(message = "CEP não pode ser nulo.")
     private String zipCode;
+    @NotNull(message = "Número não pode ser nulo.")
     private String houseNumber;
 }
